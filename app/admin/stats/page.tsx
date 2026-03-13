ca117078af9cb70af2ae220a56c5d8723c48fc30
quocnguyen1983 <quocnguyen1983@gmail.com>
2026-03-13 11:38:06 +0700

    fix railway build prisma

--- app/admin/stats/page.tsx
+++ app/admin/stats/page.tsx
@@ -1,5 +1,5 @@
 import prisma from "@/lib/prisma"
-
+export const dynamic = "force-dynamic"
 export default async function StatsPage() {
 
   const totalUsers = await prisma.user.count()
--- app/admin/users/page.tsx
+++ app/admin/users/page.tsx
@@ -1,5 +1,5 @@
 import prisma from "@/lib/prisma"
-
+export const dynamic = "force-dynamic"
 export default async function UsersPage() {
 
   const users = await prisma.user.findMany()
