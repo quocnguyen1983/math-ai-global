6190e76f8a85f21b19ad7a96ea69f001fd7ba7a7
quocnguyen1983 <quocnguyen1983@gmail.com>
2026-03-13 11:44:21 +0700

    fix admin prerender railway

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation"
 import prisma from "@/lib/prisma"
-
+export const dynamic = "force-dynamic"
 export default async function AdminPage() {
 
   // Email admin của bạn
